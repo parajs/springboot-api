367fdd06df3ad66b8f32cff80fb6d4beefb96f4f
chenguzhen87 <546369005@qq.com>
2021-05-30 02:45:25 +0800

    feat: init

---
+++ src/main/java/com/para/adminPara/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.para.adminPara.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+  }
+}
---
+++ src/test/java/com/para/adminPara/AdminParaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.para.adminPara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminParaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
